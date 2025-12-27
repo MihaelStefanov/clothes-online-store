01d447b2f17201a4bc984888e12bedee33c29806
MihaelStefanov <mihaelstefanov@abv.bg>
2025-12-28 01:35:11 +0200

    Add Item component and update Home page layout
    Introduced a new Item component with associated styles for displaying product information and an add-to-cart button. Updated the Home page to use the new Item component and applied a flexbox layout for better presentation.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,22 @@
+import "./Home.css";
+import Item from "../Item/Item";
+
 export default function Home() {
 
-
     return (
-        <h1>Home page</h1>
+        <div className="container">
+            < Item img={'https://cdn.aboutstatic.com/file/images/bbc12ef63793ba68a2928f01004ae39b.png?bg=F4F4F5&quality=75&trim=1&height=1280&width=960'}
+                title={"Ботуши"}
+                price={`${330.25}$`} />
+
+             < Item img={'https://cdn.aboutstatic.com/file/images/bbc12ef63793ba68a2928f01004ae39b.png?bg=F4F4F5&quality=75&trim=1&height=1280&width=960'}
+                title={"Ботуши"}
+                price={`${330.25}$`} />
+
+             < Item img={'https://cdn.aboutstatic.com/file/images/bbc12ef63793ba68a2928f01004ae39b.png?bg=F4F4F5&quality=75&trim=1&height=1280&width=960'}
+                title={"Ботуши"}
+                price={`${330.25}$`} />
+        </div>
     )
 
 }
---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,28 @@
+import "./Item.css";
+
+export default function Item({
+    img,
+    title,
+    price,
+    onAddToCart
+}) {
+
+    return (
+        <div className="product-item">
+            <div className="product-item-image-wrapper">
+                <img src={img} alt={title} />
+                <button
+                    className="add-to-cart-btn"
+                    onClick={onAddToCart}
+                >
+                    Добави в кошницата
+                </button>
+            </div>
+            <div className="product-item-title-wrapper">
+                <h3>{title}</h3>
+                <p className="price">{price}</p>
+            </div>
+        </div>
+    )
+
+}
