16436ffef4ee36d5e686f9c0cdc4e710e9e54fce
MihaelStefanov <mihaelstefanov@abv.bg>
2026-02-01 03:47:21 +0200

    Add initial Details page layout and structure
    Created Details.css and updated Details.jsx to include a structured layout for the product details page, with placeholders for gallery, brand, product name, price, size selection, and action buttons.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,44 @@
-export default function Details () {
-
+export default function Details() {
 
     return (
-        <h1>Details Page!</h1>
+        <section className="section-wrapper">
+            <h1>Details Page!</h1>
+            <div className="media">
+                {/* <img src={img} alt={title} /> */}
+                <h2>gallery here.</h2>
+            </div>
+
+            <div className="section-details">
+                <div className="header-details">
+                    <h2>Brand goes here.</h2>
+                    <h3>product name.</h3>
+                </div>
+
+                <div price-details>
+                    <p>price €</p>
+                </div>
+
+
+                <select className="size-select-details">
+                    <option value="S">S</option>
+                    <option value="XS">XS</option>
+                    <option value="M">M</option>
+                    <option value="L">L</option>
+                    <option value="XL">XL</option>
+                    <option value="XXL">XXL</option>
+                </select>
+
+                <div className="buttons-wrapper">
+                    <button className="add-to-bag">Add To Bag</button>
+                    <button className="add-to-wishlist">Wishlist</button>
+                </div>
+
+
+
+            </div>
+
+
+        </section>
     );
 
 }
