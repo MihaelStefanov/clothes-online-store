2fd5caf56fed59da3c356ed06b7f61365dd65b27
MihaelStefanov <mihaelstefanov@abv.bg>
2026-02-02 19:13:18 +0200

    Make Details page dynamic and improve styling
    Refactored Details.jsx to fetch and display item data dynamically based on the route parameter, including brand, name, price, color, and size options for clothing and shoes. Updated Details.css and Item.css to enhance media and image styling for better layout and responsiveness.

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,41 +1,75 @@
+import { useParams } from "react-router";
 import "./Details.css";
+import { useEffect, useState } from "react";
 
 export default function Details() {
+
+    const { itemId } = useParams();
+    const [item, setItem] = useState({});
+
+    useEffect(() => {
+        fetch(`https://clothing-store-9888e-default-rtdb.europe-west1.firebasedatabase.app/items/${itemId}.json`)
+            .then(response => response.json())
+            .then(result => setItem(result))
+            .catch(err => alert(err.message));
+
+    }, [itemId]);
+
+    if (!item.images) {
+        return <p>Loading...</p>;
+    }
 
     return (
         <section className="section-wrapper">
             <div className="media">
-                <img src="https://cdn.aboutstatic.com/file/images/0237ad04256b2f009918954d1cf3ed75.png?bg=F4F4F5&quality=75&trim=1&height=1067&width=800" alt="" />
+                <img src={item.images[0]} alt={item.name} />
             </div>
 
             <div className="section-details">
                 <div className="header-details">
-                    <h2>HUGO</h2>
-                    <h3>polo tshirt</h3>
+                    <h2>{item.brand}</h2>
+                    <h3>{item.name}</h3>
                 </div>
 
-                <div price-details>
-                    <p>price €</p>
+                <div className="price-details">
+                    <p>{item.price}€</p>
                 </div>
 
-                <select className="size-select-details">
-                    <option value="S">S</option>
-                    <option value="XS">XS</option>
-                    <option value="M">M</option>
-                    <option value="L">L</option>
-                    <option value="XL">XL</option>
-                    <option value="XXL">XXL</option>
+                 <div className="color-details">
+                    <p>Color: {item.color}</p>
+                </div>
+
+                {item.categoryIds[1] == 'clothing' ? (
+                    <select className="size-select-details" value={item.size}>
+                        <option value="S">S</option>
+                        <option value="XS">XS</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                        <option value="XXL">XXL</option>
+                    </select>
+                ) : item.categoryIds?.[1] === 'shoes' ? (
+                <select className="size-select-details" value={item.size}>
+                    <option value="38">38</option>
+                    <option value="39">39</option>
+                    <option value="40">40</option>
+                    <option value="41">41</option>
+                    <option value="42">42</option>
+                    <option value="43">43</option>
+                    <option value="44">44</option>
                 </select>
+    ) : null}
+
+
+
+
 
                 <div className="buttons-wrapper">
                     <button className="add-to-bag">Add To Bag</button>
-                    <button className="add-to-wishlist"><i class="fa-regular fa-heart"></i></button>
+                    <button className="add-to-wishlist"><i className="fa-regular fa-heart"></i></button>
                 </div>
 
-
-
             </div>
-
 
         </section>
     );
