4b3a8a27ceb1f85e6a5a87aaee1b618cfd7e0476
MihaelStefanov <mihaelstefanov@abv.bg>
2026-02-08 00:24:39 +0200

    Update MatchedItemsList.jsx

--- src/components/Matched-Items-List/MatchedItemsList.jsx
+++ src/components/Matched-Items-List/MatchedItemsList.jsx
@@ -1,4 +1,6 @@
 import "./MatchedItemsList.css";
+import { Link } from "react-router";
+
 
 export default function MatchedItemsList({
     id,
@@ -7,9 +9,11 @@ export default function MatchedItemsList({
 
     console.log(id);
     return (
+        <Link to={`/items/${id}/details`}>
         <div className="mached-items-list-media">
             <img src={item.images} alt={id} />
         </div>
+        </Link>
     )
 
 }
