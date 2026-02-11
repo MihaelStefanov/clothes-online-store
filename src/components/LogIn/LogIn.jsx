a5cdd0f2fb5e0e198416e4247c58a3d3c5511c57
MihaelStefanov <mihaelstefanov@abv.bg>
2026-02-12 01:05:53 +0200

    Update LogIn.jsx

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -2,7 +2,32 @@ export default function LogIn() {
 
 
     return (
-        <h2>LogIn</h2>
+        <div>
+            <h2>LogIn</h2>
+
+            <form action="">
+                <div>
+                    <label className="">Full Name</label>
+                    <input
+                        type="text"
+                        placeholder="Ivan Ivanov"
+                        name="name"
+                        value={''}
+                    />
+                </div>
+
+                <div>
+                    <label className="">Email Address</label>
+                    <input
+                        type="email"
+                        placeholder="example@gmail.com"
+                        name="email"
+                        value={''}
+                    />
+                </div>
+            </form>
+
+        </div>
 
     )
 }
